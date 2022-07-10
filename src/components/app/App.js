3d0295eba802e460a17c1d6a4ffe50161d28195b
Mykola Dukhnych <mykola.dukhnych@gmail.com>
2022-07-10 06:47:29 +0300

    added app settings

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import Search from '../search/Search';
 import CitiesList from '../citiesList/CitiesList';
+import Settings from '../settings/Settings';
 
 import settingsIcon from '../../resources/img/settings.svg';
 import updateIcon from '../../resources/img/reload.svg';
 import './app.scss';
 
 const App = () => {
+    if (!localStorage.getItem('cities')) {
+        localStorage.setItem('cities', JSON.stringify([]));
+    }
+    if (!localStorage.getItem('settings')) {
+        localStorage.setItem('settings', JSON.stringify({units: 'metric', autoupdate: 0, language: 'english'}));
+    }   
+
+    // eslint-disable-next-line
+    const [cities, setCities] = useState(JSON.parse(localStorage.getItem('cities')));
+    const [settings, setSettings] = useState(JSON.parse(localStorage.getItem('settings')));
+    const [showSettings, setShowSettings] = useState(false);
+
     return (
         <div className="app">
             <h1 className='main-title'>Weather App</h1>
@@ -15,12 +30,16 @@ const App = () => {
                     <button className='updateall-btn'>
                         <img src={updateIcon} alt="updateAll" />
                     </button>
-                    <button className='settings-btn'>
+                    <button className='settings-btn' onClick={() => setShowSettings(true)}>
                         <img src={settingsIcon} alt="settings" />
                     </button>
                 </div>                
             </div>            
             <CitiesList />
+            { showSettings ? <Settings 
+                                settings={settings}
+                                setSettings={setSettings}
+                                setShowSettings={setShowSettings} /> : null }
         </div>
     )
 }
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,48 +1,72 @@
-import Button from '../button/button';
+import { useState } from 'react';
+
+import Button from '../button/Button';
 
 import cross from '../../resources/img/cross.svg';
 import './settings.scss';
 
-const Settings = () => {
+const Settings = ({settings, setSettings, setShowSettings}) => {
+    const [units, setUnits] = useState(settings.units);
+    const [autoupdate, setAutoupdate] = useState(settings.autoupdate);
+    const [language, setLanguage] = useState(settings.language);
+
+    const onSaveSettings = () => {
+        localStorage.setItem('settings', JSON.stringify({units, autoupdate,language}));
+        setSettings({units, autoupdate,language});
+        setShowSettings(false);
+    }
+
     return (
         <div className="settings">
             <div className="settings-inner">
                 <div className="settings-header">
                     <h3 className='settings-title'>Settings:</h3>
-                    <button className="settings-close-btn">
+                    <button className="settings-close-btn" onClick={() => setShowSettings(false)}>
                         <img src={cross} alt="close-icon" />
                     </button>
                 </div>
                 <div className="settings-body">
                     <div className="settings-units">
                         <label htmlFor="units">Units:</label>
-                        <select name="units" id="units">
-                            <option value="metric">Metric (°C, km/h, mm)</option>
-                            <option value="imperial">Imperial (°F, mph, in)</option>
+                        <select 
+                            name="units" 
+                            id="units"
+                            value={units}
+                            onChange={e => setUnits(e.target.value)} >
+                                <option value="metric">Metric (°C, km/h, mm)</option>
+                                <option value="imperial">Imperial (°F, mph, in)</option>
                         </select>
                     </div>
                     <div className="settings-autoupdate">
                         <label htmlFor="autoupdate">Autoupdate:</label>
-                        <select name="autoupdate" id="autoupdate">
-                            <option value={0}>off</option>
-                            <option value={1}>1 hour</option>
-                            <option value={3}>3 hours</option>
-                            <option value={6}>6 hours</option>
-                            <option value={12}>12 hours</option>
-                            <option value={24}>24 hours</option>
+                        <select 
+                            name="autoupdate" 
+                            id="autoupdate"
+                            value={autoupdate}
+                            onChange={e => setAutoupdate(e.target.value)} >
+                                <option value={0}>off</option>
+                                <option value={1}>1 hour</option>
+                                <option value={3}>3 hours</option>
+                                <option value={6}>6 hours</option>
+                                <option value={12}>12 hours</option>
+                                <option value={24}>24 hours</option>
                         </select>
                     </div>
                     <div className="settings-language">
                         <label htmlFor="language">Language:</label>
-                        <select name="language" id="language">
-                            <option value={'english'}>English</option>
-                            <option value={'ukrainian'}>Ukrainian</option>
+                        <select 
+                            name="language" 
+                            id="language"
+                            value={language}
+                            onChange={e => setLanguage(e.target.value)} >
+                                <option value={'english'}>English</option>
+                                <option value={'ukrainian'}>Ukrainian</option>
                         </select>
                     </div>
                 </div>
                 <div className="settings-footer">
-                    <Button action={() => console.log('save')} classes='btn-green'>Save</Button>
-                    <Button action={() => console.log('cancel')} classes='btn-red'>Cancel</Button>
+                    <Button action={onSaveSettings} classes='btn-green'>Save</Button>
+                    <Button action={() => setShowSettings(false)} classes='btn-red'>Cancel</Button>
                 </div>
             </div>
         </div>
